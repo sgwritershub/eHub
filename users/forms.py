7499f19594b4c150f5f1232aad40cf22dae16edf
lazy_geek <sgwritershub@gmail.com>
2019-04-09 21:57:54 +0300

    a second Commit with Basic about Page and Updating the Profile Page with the ability to update it

--- users/forms.py
+++ users/forms.py
@@ -2,6 +2,7 @@ from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 from phonenumber_field.modelfields import PhoneNumberField
+from .models import Profile
 
 
 class RegistrationForm(UserCreationForm):
@@ -9,3 +10,14 @@ class RegistrationForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
+
+class UserUpdateForm(forms.ModelForm):
+    email = forms.EmailField()
+    class Meta:
+        model = User
+        fields = ['username', 'email']
+
+class ProfileUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields= ['image', 'phone', 'county']
--- users/models.py
+++ users/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
-from phonenumber_field.modelfields import PhoneNumberField
+from PIL import Image
 
 # Create your models here.
 class Profile(models.Model):
@@ -11,3 +11,12 @@ class Profile(models.Model):
 
     def __str__(self):
         return f"{ self.user.username } Profile"
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+        img = Image.open(self.image.path)
+        max_size = (250,250)
+
+        if img.height > 250 or img.width > 250:
+            img.thumbnail(max_size)
+            img.save(self.image.path)
