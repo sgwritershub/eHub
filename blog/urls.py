e85e7c82a9037681d97fc1a214a7e89479771c6d
lazy_geek <sgwritershub@gmail.com>
2019-04-13 01:46:47 +0300

    an update for the Post in the views module and repective templates and the urls module to give the posts the capability  to be deleted, updated by the very specific users who created them

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
 from . import views
+from.views import HomeListView, HomeDetailView, HomeCreateView, HomeUpdateView, HomeDeleteView
 
 urlpatterns = [
-    path('', views.home, name='blog-home'),
+    path('', HomeListView.as_view(), name='blog-home'),
+    path('post/<int:pk>/', HomeDetailView.as_view(), name='post-detail'),
+    path('post/<int:pk>/update', HomeUpdateView.as_view(), name='post-update'),
+    path('post/<int:pk>/delete', HomeDeleteView.as_view(), name='post-delete'),
+    path('post/new', HomeCreateView.as_view(), name='post-create'),
     path('about/', views.about, name='blog-about')
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Post
+from django.views.generic import ListView, DetailView, CreateView,UpdateView, DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 
 # Create your views here.
 
@@ -10,6 +12,61 @@ def home(request):
         'title': 'Home',
     }
     return render(request, 'blog/home.html', context)
+class HomeListView(ListView):
+    model = Post
+    context_object_name = 'posts'
+    template_name = 'blog/home.html'
+    ordering = ['-date_posted']
+
+class HomeDetailView(DetailView):
+    model = Post
+    template_name = 'blog/details.html'
+    context_object_name = 'post'
+
+class HomeCreateView(LoginRequiredMixin, CreateView):
+    model = Post 
+    fields = ['title', 'content']
+    template_name = 'blog/post.html'
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+class HomeUpdateView(UserPassesTestMixin, LoginRequiredMixin, UpdateView):
+    model = Post
+    #context_object_name = 'post'
+    template_name = 'blog/update.html'
+
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    def test_func(self):
+        post = self.get_object()
+
+        if self.request.user == post.author:
+            return True
+        else:
+            return False
+
+class HomeDeleteView(UserPassesTestMixin, LoginRequiredMixin, DeleteView):
+    model = Post 
+    template_name = 'blog/delete.html'
+    context_object_name = 'post'
+    success_url = '/'
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        else:
+            return False
+
+
+
+
 
 def about(request):
     context = {
